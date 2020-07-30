a127f2d2a428dbf6123f99c1d8a6a9dd0b95b089
rkasana <reena18.jnu@gmail.com>
2020-07-30 10:54:27 +0530

    updates

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def predictRoute():
 
 if __name__ == "__main__":
     clntApp = ClientApi()
-    host = '0.0.0.0'
-    port = 5000
+    #host = '0.0.0.0'
+    #port = 5000
     app.run(debug=True)
     # app.run(debug=True)
